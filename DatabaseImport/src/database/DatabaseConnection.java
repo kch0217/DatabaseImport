00bc1b7a0b5c1a2185277f7a04613c1f2e4b0ad2
Ken <kch0217@yahoo.com.hk>
2016-10-04 10:46:52 +0800

    update

--- DatabaseImport/src/database/DatabaseConnection.java
+++ DatabaseImport/src/database/DatabaseConnection.java
@@ -36,8 +36,14 @@ public class DatabaseConnection {
 		try (Connection conn = DriverManager.getConnection(Main.dbpath, Main.dbuser, Main.dbpw)) {
 			create = DSL.using(conn, SQLDialect.MYSQL);
 			BatchBindStep forbi = null;
-			if (Main.fieldName.equals("AUTH_USER_TEST")){
-				forbi =  create.batch(create.insertInto(AUTH_USER_TEST, AUTH_USER_TEST.ID, AUTH_USER_TEST.USERNAME,  AUTH_USER_TEST.FIRST_NAME, AUTH_USER_TEST.LAST_NAME, AUTH_USER_TEST.EMAIL, AUTH_USER_TEST.PASSWORD, AUTH_USER_TEST.IS_STAFF, AUTH_USER_TEST.IS_ACTIVE,  AUTH_USER_TEST.IS_SUPERUSER, AUTH_USER_TEST.LAST_LOGIN, AUTH_USER_TEST.DATE_JOINED  ).values( (Integer) null, null, null, null, null, null, null, null, null, null, null).onDuplicateKeyIgnore());
+			if (Main.fieldName.equals("AUTH_USER")){
+				forbi =  create.batch(create.insertInto(AUTH_USER, AUTH_USER.ID, AUTH_USER.USERNAME,  AUTH_USER.FIRST_NAME, AUTH_USER.LAST_NAME, AUTH_USER.EMAIL, AUTH_USER.PASSWORD, AUTH_USER.IS_STAFF, AUTH_USER.IS_ACTIVE,  AUTH_USER.IS_SUPERUSER, AUTH_USER.LAST_LOGIN, AUTH_USER.DATE_JOINED  ).values( (Integer) null, null, null, null, null, null, null, null, null, null, null).onDuplicateKeyIgnore());
+				for(Map.Entry<Integer,ArrayList<String>> entry : loadFile.records.entrySet()) {
+					  forbi.bind(entry.getValue().get(0),entry.getValue().get(1), entry.getValue().get(2), entry.getValue().get(3), entry.getValue().get(4), entry.getValue().get(5), entry.getValue().get(6), entry.getValue().get(7), entry.getValue().get(8), entry.getValue().get(9), entry.getValue().get(10));	  
+				}
+			}
+			else if (Main.fieldName.equals("AUTH_USERPROFILE")){
+				forbi =  create.batch(create.insertInto(AUTH_USERPROFILE, AUTH_USERPROFILE.ID, AUTH_USERPROFILE.USER_ID,  AUTH_USER.FIRST_NAME, AUTH_USER.LAST_NAME, AUTH_USER.EMAIL, AUTH_USER.PASSWORD, AUTH_USER.IS_STAFF, AUTH_USER.IS_ACTIVE,  AUTH_USER.IS_SUPERUSER, AUTH_USER.LAST_LOGIN, AUTH_USER.DATE_JOINED  ).values( (Integer) null, null, null, null, null, null, null, null, null, null, null).onDuplicateKeyIgnore());
 				for(Map.Entry<Integer,ArrayList<String>> entry : loadFile.records.entrySet()) {
 					  forbi.bind(entry.getValue().get(0),entry.getValue().get(1), entry.getValue().get(2), entry.getValue().get(3), entry.getValue().get(4), entry.getValue().get(5), entry.getValue().get(6), entry.getValue().get(7), entry.getValue().get(8), entry.getValue().get(9), entry.getValue().get(10));	  
 				}
